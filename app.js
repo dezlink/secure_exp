ebeac5db0d627bbb61774a10e3dcb0c846d5e733
dezlink <skv_mail@mail.ru>
2021-10-27 23:10:00 +0900

    2nd level - encrypt

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
+const encrypt = require("mongoose-encryption");
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema ={
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+var secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
