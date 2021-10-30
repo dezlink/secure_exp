18c3da39644e55db9131398f23865f1c959472a1
dezlink <skv_mail@mail.ru>
2021-10-31 00:17:56 +0900

    Use hash

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-const encrypt = require("mongoose-encryption");
-
-console.log(process.env.API_KEY);
+const md5 = require("md5");
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
@@ -17,8 +15,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +39,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -56,7 +52,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req, res){
   const login = req.body.username;
-  const passw = req.body.password;
+  const passw = md5(req.body.password);
   User.findOne({email: login}, function(err, foundUser){
       if (err){
         console.log(err);
